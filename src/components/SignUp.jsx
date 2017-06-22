256a7a1e37106f86b19e14a0fcca321906cd1cc0
Jahn <ioannis.kiwe@gmail.com>
2017-06-22 22:21:50 +0200

    src/index.js: Added dummy signup router

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+// import "./SignUp.css";
+
+class SignUp extends Component {
+    render(){
+        return (
+            <button>signin</button>
+        )
+    }
+}
+
+export default SignUp;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
+import SignUp from "./components/SignUp.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store.js";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route exact path="/signup" component={SignUp} />
+            </Switch>
+        </BrowserRouter>
     </Provider>,
   document.getElementById("root")
 );
