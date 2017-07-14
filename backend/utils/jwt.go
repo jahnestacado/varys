45a8bd2bb19a3966121a0d1a6984127ccc809cbf
Jahn <ioannis.kiwe@gmail.com>
2017-07-14 22:03:12 +0200

    backend/utils/jwt.go: Changed salt-handling mechanism to accept claim-independent values

--- backend/utils/jwt.go
+++ backend/utils/jwt.go
@@ -2,8 +2,6 @@ package utils
 
 import (
 	b64 "encoding/base64"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"strings"
 
@@ -15,36 +13,19 @@ type Salt struct {
 	Suffix string
 }
 
-func CreateToken(secret string, claims jwt.MapClaims, saltFromClaimsSpecs Salt) (string, error) {
+func CreateToken(secret string, claims jwt.MapClaims, salt Salt) (string, error) {
 	token := jwt.NewWithClaims(getSigningMethod(), claims)
-	payloadBytes, err := json.Marshal(claims)
-	if err != nil {
-		return "", err
-	}
-	salt, err := getSaltFromClaims(payloadBytes, saltFromClaimsSpecs)
-	if err != nil {
-		return "", err
-	}
 	tokenString, err := token.SignedString([]byte(getSaltedSecret(secret, salt)))
 	headerlessTokenString := getHeaderlessToken(tokenString)
 	return headerlessTokenString, err
 }
 
-func ValidateToken(secret string, headerlessTokenString string, saltFromClaimsSpecs Salt) error {
+func ValidateToken(secret string, headerlessTokenString string, salt Salt) error {
 	signingMethod := getSigningMethod()
 	tokenHeader := getB64TokenHeader(signingMethod.Alg())
 	tokenString := tokenHeader + "." + headerlessTokenString
-	payloadBytes, err := b64.RawStdEncoding.DecodeString(strings.Split(tokenString, ".")[1])
-	if err != nil {
-		return err
-	}
 
-	salt, err := getSaltFromClaims(payloadBytes, saltFromClaimsSpecs)
-	if err != nil {
-		return err
-	}
-
-	_, err = jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+	_, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
@@ -57,6 +38,7 @@ func ValidateToken(secret string, headerlessTokenString string, saltFromClaimsSp
 func getB64TokenHeader(alg string) string {
 	return b64.StdEncoding.EncodeToString([]byte(`{"alg":"` + alg + `","typ":"JWT"}`))
 }
+
 func getSigningMethod() *jwt.SigningMethodHMAC {
 	return jwt.SigningMethodHS256
 }
@@ -68,29 +50,4 @@ func getHeaderlessToken(tokenString string) string {
 
 func getSaltedSecret(secret string, salt Salt) string {
 	return salt.Prefix + secret + salt.Suffix
-}
-
-func getSaltFromClaims(payloadBytes []byte, saltFromClaimsSpecs Salt) (Salt, error) {
-	var payloadMapTemplate interface{}
-	err := json.Unmarshal(payloadBytes, &payloadMapTemplate)
-	var salt Salt
-	if err != nil {
-		return salt, err
-	}
-	payload := payloadMapTemplate.(map[string]interface{})
-	prefix, ok := payload[saltFromClaimsSpecs.Prefix].(string)
-	if !ok {
-		return salt, errors.New("Unexpected saltFromClaimsSpecs prefix type")
-
-	}
-
-	suffix, ok := payload[saltFromClaimsSpecs.Suffix].(string)
-	if !ok {
-		return salt, errors.New("Unexpected saltFromClaimsSpecs suffix type")
-
-	}
-
-	salt = Salt{prefix, suffix}
-
-	return salt, nil
 }
