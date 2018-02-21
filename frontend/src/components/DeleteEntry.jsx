c0797c1b39a08d2de5c1d77e933bf91b2b436015
Jahn <ioannis.kiwe@gmail.com>
2018-02-21 22:12:27 +0100

    frontend/src/components/DeleteEntry.jsx: Now uses semantic-ui modal for confirmation

--- frontend/src/components/DeleteEntry.jsx
+++ frontend/src/components/DeleteEntry.jsx
@@ -1,46 +1,84 @@
-import  React, { Component } from "react";
-import { Glyphicon } from "react-bootstrap";
+import React, { Component } from "react";
 import "./DeleteEntry.css";
 import bindToComponent from "./../utils/bindToComponent.js";
 import { connect } from "react-redux";
 import { deleteEntry } from "./../actions/entryActions.js";
 import handleFetchError from "./../utils/handleFetchError.js";
+import { Icon, Modal, Button, Header } from "semantic-ui-react";
 
 class DeleteEntry extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         const self = this;
-        bindToComponent(self, ["deleteEntry"]);
+        self.state = {
+            showModal: false,
+        };
+        bindToComponent(self, ["deleteEntry", "openModal", "closeModal"]);
     }
 
-    deleteEntry(event){
+    deleteEntry(event) {
         event.stopPropagation();
         console.log("Ask for Confirmation!!!!!", this.props.entry);
         const self = this;
         const { entry, deleteEntry, auth } = self.props;
         const url = "http://localhost:7676/api/v1/entry";
+
         fetch(url, {
             method: "DELETE",
-            body: JSON.stringify({id: entry.id}),
+            body: JSON.stringify({ id: entry.id }),
             headers: new Headers({
-                "Accept": "application/json",
+                Accept: "application/json",
                 "Content-Type": "application/json",
-                "JWT": auth.token,
+                JWT: auth.token,
             }),
         })
-        .then(handleFetchError)
-        .then(() => {
-            deleteEntry(entry);
-        })
-        .catch(console.log);
+            .then(handleFetchError)
+            .then(() => {
+                deleteEntry(entry);
+            })
+            .catch(console.log);
     }
 
-    render(){
+    openModal() {
+        console.log("SHOWW");
         const self = this;
+        self.setState({
+            showModal: true,
+        });
+    }
+
+    closeModal() {
+        console.log("SHOWW");
+        const self = this;
+        self.setState({
+            showModal: false,
+        });
+    }
+
+    render() {
+        const self = this;
+        const { state, props } = self;
         return (
             <div className="DeleteEntry">
-                <Glyphicon className="DeleteEntry-btn-delete" glyph="trash" onClick={self.deleteEntry} />
+                <Icon className="DeleteEntry-btn-delete" name="trash" onClick={self.openModal} />
+                <Modal
+                    open={state.showModal}
+                    onClose={this.closeModal}
+                    basic
+                    size="small"
+                    closeIcon
+                >
+                    <Header icon="browser" content={props.entry.title} />
+                    <Modal.Content>
+                        <h3>Do you really want to delete entry #{props.entry.id}?</h3>
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button color="red" onClick={self.deleteEntry} inverted>
+                            <Icon name="eraser" /> Delete
+                        </Button>
+                    </Modal.Actions>
+                </Modal>
             </div>
         );
     }
