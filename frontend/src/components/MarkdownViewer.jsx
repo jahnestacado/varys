2edd83d9ca6c6a691fad1dfc61d2b3f3c0af13de
Jahn <ioannis.kiwe@gmail.com>
2018-03-02 13:31:35 +0100

    frontend/src/components/MarkdownViewer.jsx: Now also renders the tags of the entry

--- frontend/src/components/MarkdownViewer.jsx
+++ frontend/src/components/MarkdownViewer.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import showdown from "showdown";
-import { Container } from "semantic-ui-react";
+import { Container, Label } from "semantic-ui-react";
+import "./MarkdownViewer.css";
 const converter = new showdown.Converter();
+
 const style = {
     fontSize: 14,
 };
@@ -10,9 +12,18 @@ const MarkdownViewer = ({ entry, noTitle }) => {
     const { title, body } = entry;
     const content = noTitle ? body : `# ${title} \n${body}`;
     const generatedHtml = converter.makeHtml(content);
+    // @TODO Create component that generates labels
+    const keywordLabels = entry.tags.map((keyword, i) => {
+        return (
+            <Label key={i} color="teal" size="small" className="ResultListItem-label">
+                {keyword}
+            </Label>
+        );
+    });
     return (
         <Container className="MarkdownViewer markdown-body" text style={style}>
             <div dangerouslySetInnerHTML={{ __html: generatedHtml }} />
+            <div className="MarkdownViewer-footer">{keywordLabels}</div>
         </Container>
     );
 };
