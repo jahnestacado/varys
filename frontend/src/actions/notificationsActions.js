9874354e6734ee885c3433e7f54ac64b745be86f
Jahn <ioannis.kiwe@gmail.com>
2018-03-05 01:19:25 +0100

    frontend/src/actions/notificationsActions.js: Implemented getNotifications action

--- frontend/src/actions/notificationsActions.js
+++ frontend/src/actions/notificationsActions.js
@@ -1,9 +1,25 @@
 import { SELECT_NOTIFICATION_ITEM, SET_NOTIFICATION_ITEMS } from "../utils/constants.js";
+import Http from "./../utils/http.js";
 
 export const selectNotificationItem = (notificationItem) => {
     return {
         type: SELECT_NOTIFICATION_ITEM,
         payload: notificationItem,
+    };
+};
+
+export const getNotifications = () => {
+    return (dispatch, getState) => {
+        const url = "http://localhost:7676/api/v1/notification";
+        const JWT = getState().auth.token;
+        return Http.get(url, JWT)
+            .then((notifications) => {
+                dispatch(setNotificationItems(notifications));
+            })
+            .catch((error) => {
+                // @TODO Handle error through an action
+                console.error(error);
+            });
     };
 };
 
