47e27ff0d9035cf815c30fa00e72ae42a2a1a472
Jahn <ioannis.kiwe@gmail.com>
2019-03-11 16:08:50 +0100

    frontend/src/utils/inputValidation.js: Added validatePort and validateNotEmpty functions

--- frontend/src/utils/inputValidation.js
+++ frontend/src/utils/inputValidation.js
@@ -1,31 +1,25 @@
 const InputValidation = {
     validateUsername(username) {
-        const errors = [];
-        if (!username.length) {
-            errors.push("is required");
-        } else if (username.length < 3 || username.length > 20) {
+        const errors = [].concat(InputValidation.validateNotEmpty(username));
+        if (!errors.length && (username.length < 3 || username.length > 20)) {
             errors.push("must be from 3 to 20 characters long");
         }
 
         return errors;
     },
     validateEmail(email) {
-        const errors = [];
         const emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-        if (!email.length) {
-            errors.push("is required");
-        } else if (!emailRegExp.test(email)) {
+        const errors = [].concat(InputValidation.validateNotEmpty(email));
+        if (!errors.length && !emailRegExp.test(email)) {
             errors.push("must be a valid email address");
         }
 
         return errors;
     },
     validateConfirmedPassword(password, confirmedPassword) {
-        const errors = [];
-        if (!confirmedPassword.length) {
-            errors.push("is required");
-        } else if (confirmedPassword !== password) {
+        const errors = [].concat(InputValidation.validateNotEmpty(confirmedPassword));
+        if (!errors.length && confirmedPassword !== password) {
             errors.push("must be identical to password");
         }
 
@@ -37,11 +31,8 @@ const InputValidation = {
         const containsNumber = new RegExp("\\d");
         const containsSymbol = new RegExp("[#$@!%&*?]");
 
-        const errors = [];
-
-        if (!password.length) {
-            errors.push("is required");
-        } else {
+        const errors = [].concat(InputValidation.validateNotEmpty(password));
+        if (!errors.length) {
             if (password.length < 8 || password.length > 30) {
                 errors.push("must be from 8 to 30 characters long");
             }
@@ -55,12 +46,30 @@ const InputValidation = {
                 errors.push("must contain at least one numeric character");
             }
             if (!containsSymbol.test(password)) {
-                errors.push(
-                    "must contain at least one of this symbols #$@!%&*?",
-                );
+                errors.push("must contain at least one of this symbols #$@!%&*?");
             }
         }
 
+        return errors;
+    },
+    validatePort(portAsStr) {
+        const errors = [].concat(InputValidation.validateNotEmpty(portAsStr));
+        if (!errors.length) {
+            const port = Number(portAsStr);
+            if (isNaN(port)) {
+                errors.push("must be a number");
+            } else if (port < 1 || port > 65535) {
+                errors.push("invalid port range (1 - 65535)");
+            }
+        }
+
+        return errors;
+    },
+    validateNotEmpty(str = "") {
+        const errors = [];
+        if (!str.length) {
+            errors.push("is required");
+        }
         return errors;
     },
 };
