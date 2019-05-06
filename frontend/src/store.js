3c7cae1f034747ba50a926f61ef699601901ab66
Jahn <ioannis.kiwe@gmail.com>
2019-05-06 11:34:58 +0200

    package.json: Upgraded all dependencies

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import entriesReducer from "./reducers/entriesReducer.js";
 import notificationsReducer from "./reducers/notificationsReducer.js";
 import authReducer from "./reducers/authReducer.js";
 import thunk from "redux-thunk";
+
+const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     combineReducers({
@@ -10,8 +12,7 @@ const store = createStore(
         auth: authReducer,
         notifications: notificationsReducer,
     }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk)
+    composerEnhancer(applyMiddleware(thunk))
 );
 
 export default store;
